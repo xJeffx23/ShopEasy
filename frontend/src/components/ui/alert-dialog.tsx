b91386b98343ce59f2a1523beb9a18d696f222a5
Jefferson <xjeffcmx23@gmail.com>
2026-04-14 03:55:51 -0600

    fix: correct import path for button in alert-dialog

--- frontend/src/components/ui/alert-dialog.tsx
+++ frontend/src/components/ui/alert-dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { AlertDialog as AlertDialogPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/src/components/ui/button"
 
 function AlertDialog({
   ...props
